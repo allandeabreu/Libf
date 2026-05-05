5c98f3dbd23fb4657ca069f6799b63292cb4d7dd
Allan Abreu <allandeabreucontato@gmail.com>
2026-05-05 12:58:50 +0100

    05/05/26

---
+++ ft_memcmp.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: allanbreu <allanbreu@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/05 11:46:34 by allanbreu         #+#    #+#             */
+/*   Updated: 2026/05/05 12:57:54 by allanbreu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    const unsigned char *str1;
+    const unsigned char *str2;
+    size_t i;
+
+    i = 0;
+    str1 = (const unsigned char *)s1;
+    str2 = (const unsigned char *)s2;
+
+    if (n == 0)
+        return (0);
+    while (str1[i] == str2[i] && n > 0)
+    {
+        i++;
+        n--;
+    }
+    return (str1[i] - str2[i]);
+}
+// #include <stdio.h>
+// int main()
+// {
+//     int resultado;
+
+//     resultado = ft_memcmp("hellzoo", "hello", 4);
+//     if (resultado == 0)
+//         printf("Elas sao iguais\n");
+//     else if (resultado > 0)
+//         printf("s1 eh maior que s2\n");
+//     else
+//         printf("s2 eh maior que s1\n");
+//     return (0);
+// }
